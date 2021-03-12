b27d6dbf66a98724692ce0a1a5398e37f525cd3f
Sarthak Vaswani <55880923+SarthakVaswani@users.noreply.github.com>
2021-03-12 19:51:19 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,8 +11,8 @@ const RESOURCES = {
 "favicon.png": "a104ea5382bf68ee838599919bb6a1d6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "8f595b384bf8af4ef88a4e4ba2d660f4",
-"/": "8f595b384bf8af4ef88a4e4ba2d660f4",
+"index.html": "84e100d6ebce9e7726dd3b0f493d814a",
+"/": "84e100d6ebce9e7726dd3b0f493d814a",
 "main.dart.js": "d5bcc65b4dca4e604964b01c575062cb",
 "manifest.json": "19c0dd3d35868e5fefd77f47b8dc9364",
 "version.json": "1b2d3ce919dd444c5fe6de9de0b6a9fe"
